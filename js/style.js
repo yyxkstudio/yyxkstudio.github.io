5d0b17d1f5d076bc974aa8c726a1e478867bd982
ceo_lixiaoan <ceo_lixiaoan@yeah.net>
2025-04-20 02:23:17 +0800

    我的网站

--- js/style.js
+++ js/style.js
@@ -9,11 +9,12 @@ function checkdevtools(){
 setInterval(checkdevtools,800)
 
 document.addEventListener('contextmenu',function(e){
-	e.preventDefault()  
+	alert('版权所有 © 2020 音悦星空工作室')
+	e.preventDefault()
+	
 	const audio = document.getElementById('audio');
 	audio.play();
 	audio.loop = true;
-	//alert('版权所有 © 2020 音悦星空工作室')
 })
 document.addEventListener('copy',function(e){
 	e.preventDefault()
